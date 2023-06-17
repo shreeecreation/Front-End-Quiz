e5d6694cd65d55803a28c07e63af0152eb02d17f
shreeecreation <shreeecreation720#gmail.com>
2023-06-17 15:51:41 +0545

    Feat : Project Initialize

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/src/app/screens/Home%20Screen/presentation/home_screen.dart';
+
+import 'src/core/themes/themedata.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', theme: appTheme, home: const HomeScreen());
+  }
+}
