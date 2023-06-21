ca8d66c4b8834430dd2b218fb28a7ec6bee38c9b
shreeecreation <shreeecreation720#gmail.com>
2023-06-21 07:32:09 +0545

    Feat : Added all json files

--- lib/src/app/screens/Quiz Screen/quiz.dart
+++ lib/src/app/screens/Quiz Screen/quiz.dart
@@ -8,7 +8,6 @@ import 'package:quiz/src/core/themes/appcolors.dart';
 import 'package:quiz/src/core/themes/appstyles.dart';
 
 import 'domain/quiz_domain.dart';
-import 'model/quiz_model.dart';
 
 class QuizScreen extends StatelessWidget {
   const QuizScreen({super.key});
@@ -36,26 +35,32 @@ class QuizScreen extends StatelessWidget {
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   final jsonData = json.decode(snapshot.data!);
-                  print(jsonData[0]["questions_easy"]);
                   var questionsEasy = jsonData[0]["questions_easy"];
 
                   int randomNumber = Random().nextInt(3);
                   return Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(jsonData[0]["info"][randomNumber], style: AppStyles.text18PxMedium.white),
-                      ),
+                      // ...
                       Expanded(
                         child: PageView.builder(
-                          physics: const NeverScrollableScrollPhysics(), // Disable scrolling
+                          physics: const NeverScrollableScrollPhysics(),
                           controller: page,
                           scrollDirection: Axis.horizontal,
                           pageSnapping: true,
                           itemCount: questionsEasy.length,
                           itemBuilder: (context, index) {
                             var question = questionsEasy.values.toList()[index];
-                            return buildQuestionWidget(question);
+                            return OptionWidget(
+                                question: question,
+                                onTap: () {
+                                  print("dasd");
+                                  pageIndex = index + 1;
+                                  page.animateToPage(
+                                    pageIndex,
+                                    duration: const Duration(milliseconds: 300),
+                                    curve: Curves.easeInOut,
+                                  );
+                                });
                           },
                         ),
                       ),
@@ -78,17 +83,91 @@ class QuizScreen extends StatelessWidget {
   }
 }
 
-Widget buildQuestionWidget(Map<String, dynamic> question) {
-  String questionText = question["question"];
-  List<String> options = List<String>.from(question["options"]);
-  String answer = question["answer"];
+class OptionWidget extends StatefulWidget {
+  final Map<String, dynamic> question;
+  final VoidCallback onTap;
+  const OptionWidget({Key? key, required this.question, required this.onTap}) : super(key: key);
 
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Text(questionText,style:AppStyles.text16Px.white),
-      // Display options and handle user selection
-      // ...
-    ],
-  );
+  @override
+  State<OptionWidget> createState() => _OptionWidgetState();
+}
+
+class _OptionWidgetState extends State<OptionWidget> {
+  int selectedTileIndex = -2;
+  int answerIndex = -1;
+  bool isTapped = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    answerIndex = int.parse(widget.question["answer"]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String questionText = widget.question["question"];
+    List<String> options = List<String>.from(widget.question["options"]);
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height / 1.5,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(questionText, style: AppStyles.text18Px.white, textAlign: TextAlign.center),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 320,
+              child: Card(
+                color: AppColors.cardColor,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 8.0),
+                  child: GestureDetector(
+                    onTap: widget.onTap,
+                    child: ListView.builder(
+                      itemCount: options.length,
+                      itemExtent: 75,
+                      itemBuilder: (context, index) {
+                        bool isCorrectAnswer = answerIndex == index;
+                        bool isSelected = selectedTileIndex == index;
+                        bool isIncorrectSelection = isSelected && !isCorrectAnswer;
+                        Color unTappedColor = const Color.fromARGB(138, 0, 0, 0);
+                        Color tileColor = isIncorrectSelection
+                            ? Colors.red
+                            : isSelected || isCorrectAnswer
+                                ? Colors.green
+                                : const Color.fromARGB(138, 0, 0, 0);
+
+                        return Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Container(
+                            decoration: BoxDecoration(color: isTapped ? tileColor : unTappedColor, borderRadius: BorderRadius.circular(8.0)),
+                            child: ListTile(
+                              title: Text(options[index], style: AppStyles.text18Px.white),
+                              onTap: !isTapped
+                                  ? () {
+                                      setState(() {
+                                        widget.onTap;
+                                        isTapped = true;
+                                        selectedTileIndex = index;
+                                      });
+                                    }
+                                  : null,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
 }
